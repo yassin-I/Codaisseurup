54e800a39459baed762bc0f7ab2a8ed4a00d67eb
ISMAIL YASSIN <YISMAIL299@GMAIL.COMssh-keygen -t rsa -C ISMAIL>
2017-07-10 16:12:45 +0200

    Add API events index

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
-  belongs_to :user, dependent: :destroy
+  belongs_to :user, optional: true
   has_and_belongs_to_many :categories, dependent: :destroy
   has_many :photos, dependent: :destroy
   has_many :bookings, dependent: :destroy
